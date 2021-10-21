c27d3a863125fb9b73624cc82de9f96758b482fd
chazzox <42206116+chazzox@users.noreply.github.com>
2021-10-21 22:49:46 +0100

    Added project boilerplate

---
+++ plugin/groupdm.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name groupdm
+ * @author chazzox#1001
+ * @description Group your Dms and group chats separately
+ * @version 0.0.1
+ * @website https://github.com/chazzox/groupdm#readme
+ * @source https://github.com/chazzox/groupdm
+ */
+BdApi;module.exports=class{load(){}start(){}stop(){}};
---
+++ src/groupdm.jsx
@@ -0,0 +1,9 @@
+// useful BdApi stuff
+const { React, ReactDOM } = BdApi;
+import './index.scss';
+
+module.exports = class groupdm {
+	load() {}
+	start() {}
+	stop() {}
+};

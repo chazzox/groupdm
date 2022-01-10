e6de32fe2d0daed9a6aa1925a7057b574a645f18
chazzox <42206116+chazzox@users.noreply.github.com>
2022-01-10 05:27:17 +0000

    Moved

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # groupdm
+
+## [MOVED HERE](https://github.com/chazzox/BetterDiscordPlugins/tree/main/groupdm)
+
 A better discord plugin that groups your DMs and Group Chats into their own sections
--- src/@types/actions.d.ts
+++
@@ -1,8 +0,0 @@
-interface CHANNEL_SELECT_ACTION {
-	channelId?: string | null;
-	guildId: string | null;
-	hoistedRoute: undefined;
-	jumpType: undefined;
-	messageId: string | null;
-	type: 'CHANNEL_SELECT';
-}
